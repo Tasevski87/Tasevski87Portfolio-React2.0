98cc8c290196633da619d7a8ec80a5b3f48983c3
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-04-19 10:47:09 -0400

    starter code

---
+++ src/components/Hero/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+import profileImg from "./profile.jpg";
+function Hero() {
+    return (
+        <div>
+            <img src={profileImg} alt="toshe tasevski" id="hero-img" />
+            <div id="main-text-container">
+                <h1 className="main-text">Hello, I'm Pavel!</h1>
+                {/* <h2 className="main-text"></h2> */}
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import Hero from "../components/Hero";
+
+function Home() {
+    return (
+        <div id="home-page">
+            <Hero></Hero>
+        </div>
+    );
+}
+
+export default Home;
