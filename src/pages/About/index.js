bcf3ec50dfc4475820d5d0bdb625f26e2f9907ce
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-05-02 14:52:26 -0400

    about section

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -18,11 +18,10 @@ function About() {
                         <div className="col-lg-6" data-aos="fade-up">
                             <h3 className="about-title">Introduction</h3>
                             <div className="about-item pb-0">
-                              
+                            
                                 <p>
                                     <em>
-                                    Solutions-driven professional with front and back web development. Adept at offering cutting edge engineering solutions. Skilled in managing all aspects of the web development life cycle. Proficient in writing code, developing databases, creating user interfaces, and staying up to date with the newest libraries.
-                                        <br />
+                                    Hello, my name is Toshe. I was born and raised in the Republic of Macedonia in Europe. I moved to the United States in July, 2008. My wife and I live in Columbus, Ohio. I recently graduated from the Ohio State Coding Boot Camp. I am looking for a company where I can grow as a more advanced software developer.                                    <br />
                                     </em>
                                 </p>
                             
